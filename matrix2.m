250c718597ae2aa5fb4c3d0d209c6393ade2fec4
louWaldo <104614342+louWaldo@users.noreply.github.com>
2023-02-08 02:17:11 -0600

    Update matrix2.m

--- matrix2.m
+++ matrix2.m
@@ -2,48 +2,78 @@
 % between a and b
 % r = a + (b-a).*rand(N,N) 
 % in this case, a = -0.4 and b = 0.8
-A = -4 + (0.8+0.4)*rand(500,500);
-z=ones(500,1);
-b=A*z;
-
+A1 = -4 + (0.8+0.4)*rand(500,500);
+z1=ones(500,1);
+b1=A1*z1;
 % Solve Equation by partial pivoting
-fprintf('SOLVING WITH PARTIAL PIVOT \n');
+fprintf('SOLVING WITH PARTIAL PIVOT, double precision \n');
 tic
-x = partial_pivot(A,b);
+x1 = partial_pivot(A1,b1);
 toc
-smse = sqrt(sum((x-z).^2));
-fprintf('\nSMSE = %.32f\n',smse)
+smse_1 = sqrt(sum((x1-z1).^2));
+fprintf('SMSE = %.32f\n',smse_1)
+fprintf('-------------------------------------------------------------------------------\n');
+
+A_S1 = single(-4 + (0.8+0.4)*rand(500,500));
+z_S1=single(ones(500,1));
+b_S1=single(A_S1*z_S1);
+fprintf('SOLVING WITH PARTIAL PIVOT, single precision \n');
+tic
+x_S1 = single(partial_pivot(A_S1,b_S1));
+toc
+smse_S1 = single(sqrt(sum((x_S1-z_S1).^2)));
+fprintf('SMSE = %.32f\n',smse_S1)
 
 fprintf('\n==================================================================================\n\n');
 
+
 % Solving without partial pivoting
+A2 = -4 + (0.8+0.4)*rand(500,500);
+z2=ones(500,1);
+b2=A2*z2;
 fprintf('SOLVING WITHOUT PARTIAL PIVOT \n')
 tic
-x = no_partial_pivot(A,b);
+x2 = no_partial_pivot(A2,b2);
 toc
-smse = sqrt(sum((x-z).^2));
-fprintf('\nSMSE = %.32f\n',smse)
+smse_2 = sqrt(sum((x2-z2).^2));
+fprintf('SMSE = %.32f\n',smse_2)
+fprintf('-------------------------------------------------------------------------------\n');
+
+A_S2 = single(-4 + (0.8+0.4)*rand(500,500));
+z_S2=single(ones(500,1));
+b_S2=single(A_S2*z_S2);
+fprintf('SOLVING WITHOUT PARTIAL PIVOT, single precision \n');
+tic
+x_S2 = single(partial_pivot(A_S2,b_S2));
+toc
+smse_S2 = single(sqrt(sum((x_S2-z_S2).^2)));
+fprintf('SMSE = %.32f\n',smse_S2)
 fprintf('\n==================================================================================\n\n');
 
 
 
 %Solving equation with linsolve
-A = -4 + (0.8+0.4)*rand(500,500);
-z=ones(500,1);
-b=A*z;
-
-fprintf('SOLVING WITH LINSOLVE, double precision (default) \n')
+A3 = -4 + (0.8+0.4)*rand(500,500);
+z3=ones(500,1);
+b3=A3*z3;
+fprintf('SOLVING WITH LINSOLVE, double precision \n')
 tic;
-x = linsolve(A,b);
+x3 = linsolve(A3,b3);
 toc;
-smse = sqrt(sum((x-z).^2));
-fprintf('\nSMSE = %.32f\n',smse)
+smse_3 = sqrt(sum((x3-z3).^2));
+fprintf('SMSE = %.32f\n',smse_3)
 
-fprintf('SOLVING WITH LINSOLVE, single precision (default) \n')
+fprintf('-------------------------------------------------------------------------------\n');
+
+fprintf('SOLVING WITH LINSOLVE, single precision \n')
+A_S3 = single(-4 + (0.8+0.4)*rand(500,500));
+z_S3=single(ones(500,1));
+b_S3=single(A_S3*z_S3);
 tic
-single(x) = single(linsolve(A, b));
+x_S3 = single(linsolve(A_S3,b_S3));
 toc
-single(smse) = single(sqrt(sum((x-z).^2)));
+smse_S3 = single(sqrt(sum((x_S3-z_S3).^2)));
+fprintf('SMSE = %.32f\n',smse_S3)
 
 
 
